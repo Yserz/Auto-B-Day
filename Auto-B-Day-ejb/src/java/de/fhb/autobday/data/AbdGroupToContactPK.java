2d3ad534794418c17a27a9c137f5627ae1f9fe6c
TinoReuschel <reuschel@fh-brandenburg.de>
2011-11-29 18:43:11 +0100

    Rename Filenames die zweite

--- Auto-B-Day-ejb/src/java/de/fhb/autobday/data/AbdGroupToContactPK.java
+++ Auto-B-Day-ejb/src/java/de/fhb/autobday/data/AbdGroupToContactPK.java
@@ -17,6 +17,10 @@ import javax.validation.constraints.Size;
  */
 @Embeddable
 public class AbdGroupToContactPK implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 255)

c21856e83bf038233a4250779455d3959742ace4
Yserz <michael.koppen@googlemail.com>
2011-12-07 00:16:01 +0100

    Ant-Script modified, libs for ant-script added

--- Auto-B-Day-war/src/java/de/fhb/autobday/beans/CalendarBean.java
+++ Auto-B-Day-war/src/java/de/fhb/autobday/beans/CalendarBean.java
@@ -1,11 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.fhb.autobday.beans;
 
-import java.util.Date;  
-  
+import java.util.Date;
+
 import org.primefaces.event.DateSelectEvent;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
@@ -16,35 +12,33 @@ import javax.enterprise.context.Dependent;
  */
 @Named(value = "calendarBean")
 @Dependent
-public class CalendarBean {  
-  
-    private Date date1;  
-      
-    private Date date2;  
-      
-    private Date date3;  
-      
-    public Date getDate1() {  
-        return date1;  
-    }  
-  
-    public void setDate1(Date date1) {  
-        this.date1 = date1;  
-    }  
-  
-    public Date getDate2() {  
-        return date2;  
-    }  
-  
-    public void setDate2(Date date2) {  
-        this.date2 = date2;  
-    }  
-      
-    public Date getDate3() {  
-        return date3;  
-    }  
-  
-    public void setDate3(Date date3) {  
-        this.date3 = date3;  
-    }  
-}  
+public class CalendarBean {
+
+	private Date date1;
+	private Date date2;
+	private Date date3;
+
+	public Date getDate1() {
+		return date1;
+	}
+
+	public void setDate1(Date date1) {
+		this.date1 = date1;
+	}
+
+	public Date getDate2() {
+		return date2;
+	}
+
+	public void setDate2(Date date2) {
+		this.date2 = date2;
+	}
+
+	public Date getDate3() {
+		return date3;
+	}
+
+	public void setDate3(Date date3) {
+		this.date3 = date3;
+	}
+}

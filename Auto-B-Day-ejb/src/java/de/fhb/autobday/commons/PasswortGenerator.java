5b30fce65b97889e896f0539f70c61d2d9077f4a
Salaxy <salaxy@web.de>
2011-12-17 11:36:13 +0100

    Workaround PasswortGenerator

--- Auto-B-Day-ejb/src/java/de/fhb/autobday/commons/PasswortGenerator.java
+++ Auto-B-Day-ejb/src/java/de/fhb/autobday/commons/PasswortGenerator.java
@@ -1,25 +1,32 @@
 package de.fhb.autobday.commons;
 
 /**
+ * This class generates passwords
+ * and signs for security affairs.
  *
- * @author Tino Reuschel
- * @author Michael Koppen <koppen@fh-brandenburg.de>
+ *
+ * @author 
+ * Tino Reuschel <>
+ * Michael Koppen <koppen@fh-brandenburg.de>
+ * Andy Klay <klay@fh-brandenburg.de>
+ * 
  */
 public class PasswortGenerator {
 
 	public static String generatePassword() {
+		
 		final int PASSWORD_LENGTH = 8;
 		final int ALPHABET_LENGTH = 26;
 		final int ASCII_POSITION = 97;
-		
-		StringBuffer newPW = new StringBuffer();
-		for (int x = 0; x < PASSWORD_LENGTH; x++) {
-			newPW.append((char) ((int) (Math.random() * ALPHABET_LENGTH) + ASCII_POSITION));
-		}
-		return newPW.toString();
+
+		return generateSigns();
 	}
 	
 	public static String generateSalt() {
+		
+		final int PASSWORD_LENGTH = 8;
+		final int ALPHABET_LENGTH = 26;
+		final int ASCII_POSITION = 97;
 		
 		//TODO implementieren
 //		kleine buchstaben
@@ -27,19 +34,26 @@ public class PasswortGenerator {
 //		zahlen von 0-9
 //		alles an willkürlichen stellen
 
-		return "";
+		return generateSigns();
 	}
 	
-//	public static String generatePassword2(){
-//		return generateSigns();
-//		}
-//		public static String generateSalt(){
-//		return generateSigns();
-//		}
-//		private static String generateSigns(){
-//		//TODO generate signs
-//		return "";
-//		}
+
+	
+	private static String generateSigns(){
+		
+		final int PASSWORD_LENGTH = 8;
+		final int ALPHABET_LENGTH = 26;
+		final int ASCII_POSITION = 97;
+		
+		StringBuffer newPW = new StringBuffer();
+		
+		//generating
+		for (int x = 0; x < PASSWORD_LENGTH; x++) {
+			newPW.append((char) ((int) (Math.random() * ALPHABET_LENGTH) + ASCII_POSITION));
+		}
+		
+		return newPW.toString();	
+	}
 	
 	
 //	entweder legst du da als attribute konstanten fest, die die länge des pws usw festsetzt

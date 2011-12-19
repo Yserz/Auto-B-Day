73588fc0c8bfc81650083d9f2830d79006d04987
Salaxy <salaxy@web.de>
2011-12-19 01:12:23 +0100

    imports edited

--- Auto-B-Day-ejb/test/de/fhb/autobday/manager/ABDManagerTest.java
+++ Auto-B-Day-ejb/test/de/fhb/autobday/manager/ABDManagerTest.java
@@ -1,7 +1,5 @@
 package de.fhb.autobday.manager;
 
-import java.util.List;
-import javax.ejb.embeddable.EJBContainer;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.*;

09d2145b1c14cc5f530b461e35206e55b855918d
Salaxy <salaxy@web.de>
2011-12-21 21:06:13 +0100

    three new accountManager methods

--- Auto-B-Day-ejb/src/java/de/fhb/autobday/manager/account/AccountManager.java
+++ Auto-B-Day-ejb/src/java/de/fhb/autobday/manager/account/AccountManager.java
@@ -1,18 +1,25 @@
 package de.fhb.autobday.manager.account;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
 import de.fhb.autobday.dao.AbdAccountFacade;
+import de.fhb.autobday.dao.AbdGroupFacade;
 import de.fhb.autobday.dao.AbdUserFacade;
 import de.fhb.autobday.data.AbdAccount;
+import de.fhb.autobday.data.AbdContact;
+import de.fhb.autobday.data.AbdGroup;
+import de.fhb.autobday.data.AbdGroupToContact;
 import de.fhb.autobday.data.AbdUser;
 import de.fhb.autobday.exception.account.AccountException;
 import de.fhb.autobday.exception.account.AccountNotFoundException;
 import de.fhb.autobday.exception.user.UserNotFoundException;
 import de.fhb.autobday.manager.connector.google.GoogleImporter;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
 
 /**
  * The AccountManager processes all accountData specific things.
@@ -32,6 +39,9 @@ public class AccountManager implements AccountManagerLocal {
 	
 	@EJB
 	private AbdUserFacade userDAO;
+	
+	@EJB
+	private AbdGroupFacade groupDAO;
 	
 	public AccountManager() {
 	}
@@ -113,5 +123,94 @@ public class AccountManager implements AccountManagerLocal {
 		importer.importContacts();
 	}
 	
-	//TODO add method public List<AbdAccount> getAllAccounts(AbdUser user)
+	
+
+	
+	/**
+	 * 
+	 * @param accountInputObject
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public List<AbdGroup> getAllGroupsFromAccount(AbdAccount accountInputObject) throws Exception{
+		
+		AbdAccount account=null;
+		ArrayList<AbdGroup> outputCollection=new ArrayList<AbdGroup>();
+		
+		//find object, verify input
+		account=accountDAO.find(accountInputObject);
+		
+		if(account==null){
+			//TODO exception verbessern
+			LOGGER.log(Level.SEVERE, "inputobject does not exist!");
+			throw new Exception("inputobject does not exist!");
+		}
+		
+		for(AbdGroup actualGroup :account.getAbdGroupCollection()){
+			outputCollection.add(actualGroup);
+		}
+		
+		return outputCollection;
+	}
+	
+	
+	/**
+	 * 
+	 * @param userInputObject
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public List<AbdAccount> getAllAccounts(AbdUser userInputObject) throws Exception{
+		
+		AbdUser user=null;
+		ArrayList<AbdAccount> outputCollection=new ArrayList<AbdAccount>();
+		
+		//find object, verify input
+		user=userDAO.find(userInputObject);
+		
+		if(user==null){
+			//TODO exception verbessern
+			LOGGER.log(Level.SEVERE, "inputobject does not exist!");
+			throw new Exception("inputobject does not exist!");
+		}
+		
+		for(AbdAccount actualAccount :user.getAbdAccountCollection()){
+			outputCollection.add(actualAccount);
+		}
+		
+		
+		return outputCollection;
+	}
+	
+	/**
+	 * 
+	 * @param groupInputObject
+	 * @return
+	 * @throws Exception 
+	 */
+	@Override
+	public List<AbdContact> getAllContactsFromGroup(AbdGroup groupInputObject) throws Exception{
+		
+		AbdGroup group=null;
+		ArrayList<AbdContact> outputCollection=new ArrayList<AbdContact>();
+		
+		//find object, verify input
+		group=groupDAO.find(groupInputObject);
+		
+		if(group==null){
+			//TODO exception verbessern
+			LOGGER.log(Level.SEVERE, "inputobject does not exist!");
+			throw new Exception("inputobject does not exist!");
+		}
+		
+		for(AbdGroupToContact actualGroupToContact :group.getAbdGroupToContactCollection()){
+			outputCollection.add(actualGroupToContact.getAbdContact());
+		}
+		
+		
+		return outputCollection;
+	}
+
 }
--- Auto-B-Day-ejb/src/java/de/fhb/autobday/manager/account/AccountManagerLocal.java
+++ Auto-B-Day-ejb/src/java/de/fhb/autobday/manager/account/AccountManagerLocal.java
@@ -1,10 +1,16 @@
 package de.fhb.autobday.manager.account;
 
+import java.util.List;
+
+import javax.ejb.Local;
+
+import de.fhb.autobday.data.AbdAccount;
+import de.fhb.autobday.data.AbdContact;
+import de.fhb.autobday.data.AbdGroup;
+import de.fhb.autobday.data.AbdUser;
 import de.fhb.autobday.exception.account.AccountException;
 import de.fhb.autobday.exception.account.AccountNotFoundException;
 import de.fhb.autobday.exception.user.UserNotFoundException;
-
-import javax.ejb.Local;
 
 /**
  *
@@ -20,5 +26,11 @@ public interface AccountManagerLocal {
 	void removeAccount(int accountId) throws AccountException;
 
 	void importGroupsAndContacts(int accountId) throws AccountNotFoundException ;
+	
+	List<AbdGroup> getAllGroupsFromAccount(AbdAccount accountInputObject) throws Exception;
+	
+	List<AbdAccount> getAllAccounts(AbdUser userInputObject) throws Exception;
+	
+	List<AbdContact> getAllContactsFromGroup(AbdGroup groupInputObject) throws Exception;
 	
 }

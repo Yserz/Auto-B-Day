2fb4964840e3fb6e9d16b6bf7c626ecfa8b14710
Salaxy <salaxy@web.de>
2012-01-07 16:21:27 +0100

    content deleted

--- Auto-B-Day-ejb/test-integration/de/fhb/autobday/manager/contact/ContactManagerTestIntegration.java
+++ Auto-B-Day-ejb/test-integration/de/fhb/autobday/manager/contact/ContactManagerTestIntegration.java
@@ -19,59 +19,19 @@ public class ContactManagerTestIntegration {
 	
 	private AccountManager managerUnderTest;
 	
-	//private Server server;
 	private Connection con;
 	
 	@Before
 	public void setUp() {
-		
-		 /*server = new Server();
-	        server.setAddress("localhost");
-	        server.setDatabaseName(0, "db");
-	        server.setDatabasePath(0, "file:./testdb/db");
-	        server.setPort(1234);
-	        server.setTrace(true);
-	        server.setLogWriter(new PrintWriter(System.out));
-	        server.start();
-			* 
-			*/
-	        try {
-	            Class.forName("org.hsqldb.jdbc.JDBCDriver");
-	        } catch (ClassNotFoundException e) {
-	            e.printStackTrace(System.out);
-	        }
-	        try {
-	            con = DriverManager.getConnection(
-	                    "jdbc:hsqldb:hsql://localhost:1234/db", "SA", "");
-	            con.createStatement()
-	                    .executeUpdate(
-	                            "create table contacts (name varchar(45),email varchar(45),phone varchar(45))");
-	        } catch (SQLException e) {
-	            e.printStackTrace(System.out);
-	        }
-		
-		gloss= new JavaEEGloss();
-		gloss.addEJB(AbdUserFacade.class);
-		gloss.addEJB(AbdAccountFacade.class);
-		managerUnderTest=gloss.make(AccountManager.class);
-		//set Objekts to inject
+
 	}
+	
 	/**
 	 * Test of addAccount method, of class AccountManager.
 	 */
 	@Test
 	public void testAddAccount() throws Exception {
 		
-		System.out.println("addAccount");
-		
-		//prepare test variables
-		String password="password";
-		String userName="mustermann";
-		String type="type";
-		
-		
-		// testing Methodcall
-		//managerUnderTest.addAccount(1, password, userName, type);
 		
 
 	}

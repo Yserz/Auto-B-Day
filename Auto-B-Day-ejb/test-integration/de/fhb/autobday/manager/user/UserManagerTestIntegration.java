9892f4c763447a2cb5917aa1fc874d2f90a20b21
Christoph Ott <ott@fh-brandenburg.de>
2012-01-08 22:59:19 +0100

    UserManagerTestIntegration completed

--- Auto-B-Day-ejb/test-integration/de/fhb/autobday/manager/user/UserManagerTestIntegration.java
+++ Auto-B-Day-ejb/test-integration/de/fhb/autobday/manager/user/UserManagerTestIntegration.java
@@ -1,19 +1,31 @@
 package de.fhb.autobday.manager.user;
 
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
+import static org.easymock.EasyMock.*;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.stvconsultants.easygloss.javaee.JavaEEGloss;
 
+import de.fhb.autobday.commons.HashHelper;
+import de.fhb.autobday.commons.PasswordGenerator;
 import de.fhb.autobday.dao.AbdAccountFacade;
 import de.fhb.autobday.dao.AbdUserFacade;
+import de.fhb.autobday.data.AbdAccount;
+import de.fhb.autobday.data.AbdUser;
 import de.fhb.autobday.manager.connector.google.GoogleImporter;
 
 /**
@@ -29,19 +41,15 @@ public class UserManagerTestIntegration {
 	
 	private static UserManager managerUnderTest;
 	
-	private static AbdAccountFacade accountDAO;
-	
 	private static AbdUserFacade userDAO;
-	
-	private static GoogleImporter gImporter;
 	
 	private EntityManager emMock;
 	
+	private AbdUser user;
+	
 	@BeforeClass
 	public static void setUpClass(){
-		accountDAO = new AbdAccountFacade();
 		userDAO = new AbdUserFacade();
-		gImporter = new GoogleImporter();
 	}
 	
 	@Before
@@ -53,16 +61,24 @@ public class UserManagerTestIntegration {
 		emMock = createMock(EntityManager.class);
 		
 		//set EntityManagers
-		accountDAO.setEntityManager(emMock);
 		userDAO.setEntityManager(emMock);
 		
 		//set Objekts to inject
-		gloss.addEJB(accountDAO);
 		gloss.addEJB(userDAO);
-		gloss.addEJB(gImporter);
 		
 		//create Manager with Mocks
 		managerUnderTest=gloss.make(UserManager.class);
+		
+		String salt = PasswordGenerator.generateSalt();
+		String password = "";
+		try {
+			password = HashHelper.calcSHA1("1234abcd"+salt);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		user = new AbdUser(1, "max", password, salt, "mustermann", "max");
 	}
 
 
@@ -74,7 +90,7 @@ public class UserManagerTestIntegration {
 	public void testGetUser()throws Exception {
 		System.out.println("");
 		
-		//TODO implement
+		expect(emMock.find(AbdUser.class, user.getId())).andReturn(user);
 		
 		//prepare test variables
 		
@@ -82,6 +98,8 @@ public class UserManagerTestIntegration {
 		
 		// Setup is finished need to activate the mock
 		replay(emMock);
+		
+		assertEquals(user, managerUnderTest.getUser(user.getId()));
 		
 		//verify
 		verify(emMock);
@@ -92,17 +110,21 @@ public class UserManagerTestIntegration {
 	public void testLogin()throws Exception {
 		System.out.println("");
 		
-		//TODO implement
-		
-		//prepare test variables
-		
 		// Setting up the expected value of the method call of Mockobject
+		Query queryMock = createMock(Query.class);
+		expect(queryMock.setParameter("username", user.getUsername())).andReturn(queryMock);
+		expect(queryMock.getSingleResult()).andReturn(user);
+		expect(emMock.createNamedQuery("AbdUser.findByUsername")).andReturn(queryMock);
 		
 		// Setup is finished need to activate the mock
 		replay(emMock);
+		replay(queryMock);
+		
+		assertEquals(user, managerUnderTest.login(user.getUsername(), "1234abcd"));
 		
 		//verify
 		verify(emMock);
+		verify(queryMock);
 	}
 	
 
@@ -110,18 +132,31 @@ public class UserManagerTestIntegration {
 	public void testRegister()throws Exception {
 		System.out.println("");
 		
-		
-		//TODO implement
-		
 		//prepare test variables
+		String firstName = "maja";
+		String name = "biene";
+		String username = "diemaja";
+		String mail = "maja@gmail.com";
+		String password = "1234abcd";
+		
+		AbdUser expectedUser = new AbdUser(Integer.SIZE);
 		
 		// Setting up the expected value of the method call of Mockobject
+		Query queryMock = createMock(Query.class);
+		expect(queryMock.setParameter("username", username)).andReturn(queryMock);
+		expect(queryMock.getSingleResult()).andReturn(null);
+		expect(emMock.createNamedQuery("AbdUser.findByUsername")).andReturn(queryMock);
+		
+		emMock.persist((AbdUser)anyObject());
 		
 		// Setup is finished need to activate the mock
 		replay(emMock);
+		replay(queryMock);
 		
+		assertEquals(expectedUser, managerUnderTest.register(firstName, name, username, mail, password, password));
 		//verify
 		verify(emMock);
+		verify(queryMock);
 	}
 	
 
@@ -129,15 +164,24 @@ public class UserManagerTestIntegration {
 	public void testGetAllAccountsFromUser()throws Exception {
 		System.out.println("");
 		
-		
-		//TODO implement
-		
 		//prepare test variables
+		AbdAccount accountOne = new AbdAccount(1);
+		AbdAccount accountTwo = new AbdAccount(2);
+				
+		List<AbdAccount> result;
+		ArrayList<AbdAccount> outputCollection=new ArrayList<AbdAccount>();
+		outputCollection.add(accountOne);
+		outputCollection.add(accountTwo);
+				
+		user.setAbdAccountCollection(outputCollection);
 		
 		// Setting up the expected value of the method call of Mockobject
-		
+		expect(userDAO.find(user.getId())).andReturn(user);
+		userDAO.refresh(user);
 		// Setup is finished need to activate the mock
 		replay(emMock);
+		
+		assertEquals(outputCollection, managerUnderTest.getAllAccountsFromUser(user));
 		
 		//verify
 		verify(emMock);
